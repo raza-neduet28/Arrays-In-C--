c9dd6c0733d9dcfe8baf35d0886dfd61db3a6684
Raza-neduet <raza.neduet28@gmail.com>
2025-04-18 15:59:26 +0500

    Changes

---
+++ Element sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int sum=0;
+    int ar[n];
+    for(int i=0 ;i<n;i++){
+    cin>>ar[i];
+}
+for(int i=0 ;i<n;i++){
+    sum+=ar[i];
+
+}
+cout<< sum;
+}
